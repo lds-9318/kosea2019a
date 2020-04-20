0fe6cdd19f1ac01e338108c2f33d7c96414bdd6d
lds-9318 <63996987+lds-9318@users.noreply.github.com>
2020-04-20 15:31:43 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("백준짱");
-		System.out.println("치킨냄새 좋다");
+		System.out.println("수정");
+		System.out.println("해봄");
 	}
 }
